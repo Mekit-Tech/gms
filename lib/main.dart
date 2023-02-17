8db63b83a001a84b2d182f4a64578e4287546eb3
Vedant Suryawanshi <68188208+VedantSuryawanshi@users.noreply.github.com>
2023-02-17 14:08:20 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mekit_gms/UI/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: HomeScreen(),
     );
   }
 }
