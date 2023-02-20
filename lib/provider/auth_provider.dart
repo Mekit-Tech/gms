ae37fb8fc50a59ad97e428ffe396fbf03ea6b8f5
Vedant Suryawanshi <68188208+VedantSuryawanshi@users.noreply.github.com>
2023-02-21 04:21:29 +0530

    commit

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class AuthProvider extends ChangeNotifier{
-  
+class AuthProvider extends ChangeNotifier {
+  bool _isSignedIn = false;
+  bool get isSignedIn => _isSignedIn;
+
+  AuthProvider() {
+    checkSign();
+  }
+
+  void checkSign() async {
+    final SharedPreferences s = await SharedPreferences.getInstance();
+    _isSignedIn = s.getBool("is_signedin") ?? false;
+    notifyListeners();
+  }
 }
