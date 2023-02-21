160cd980713999ba5ad0e1214807668f10da4933
Vedant Suryawanshi <68188208+VedantSuryawanshi@users.noreply.github.com>
2023-02-21 18:31:15 +0530

    commit

--- lib/UI/screens/onboarding/garage_onboarding_screen.dart
+++ lib/UI/screens/onboarding/garage_onboarding_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -8,8 +11,125 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  File? logo;
+  final nameController = TextEditingController();
+  final addressController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    addressController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        toolbarHeight: 70,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: false,
+        title: SizedBox(
+          width: 45,
+          child: Image.asset('assets/icons/mekitblacklogo.png'),
+        ),
+      ),
+      floatingActionButton: SizedBox(
+        width: 200,
+        child: FloatingActionButton(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          splashColor: Colors.greenAccent.shade700,
+          hoverColor: Colors.grey,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          onPressed: () {},
+          child: const Icon(
+            Icons.arrow_forward,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: logo == null
+                    ? const CircleAvatar(
+                        backgroundColor: Colors.black87,
+                        radius: 40,
+                        child: Icon(
+                          Icons.add_a_photo_outlined,
+                          size: 25,
+                          color: Colors.white,
+                        ),
+                      )
+                    : CircleAvatar(
+                        backgroundImage: FileImage(logo!),
+                        radius: 40,
+                      ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                margin: const EdgeInsets.only(top: 20),
+                child: Column(
+                  children: [
+                    // Garage Name Field
+                    TextFormField(
+                      controller: nameController,
+                      keyboardType: TextInputType.name,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                const BorderSide(color: Colors.black26)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                const BorderSide(color: Colors.black26)),
+                        hintText: 'Mekit Garage',
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 10,
+                    ),
+
+                    // Address Field
+                    TextFormField(
+                      controller: addressController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                const BorderSide(color: Colors.black26)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                const BorderSide(color: Colors.black26)),
+                        hintText:
+                            '601, C1, Niraj City, Godrej Hills, Kalyan West',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
