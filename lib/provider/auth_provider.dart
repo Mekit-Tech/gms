5f8cbf1a71bc740ddfa83f47d3ee943e81746d43
Vedant Suryawanshi <68188208+VedantSuryawanshi@users.noreply.github.com>
2023-02-21 05:33:21 +0530

    commit

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mekit_gms/utils/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider extends ChangeNotifier {
   bool _isSignedIn = false;
   bool get isSignedIn => _isSignedIn;
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   AuthProvider() {
     checkSign();
@@ -13,5 +17,22 @@ class AuthProvider extends ChangeNotifier {
     final SharedPreferences s = await SharedPreferences.getInstance();
     _isSignedIn = s.getBool("is_signedin") ?? false;
     notifyListeners();
+  }
+
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
+    try {
+      await _firebaseAuth.verifyPhoneNumber(
+          verificationCompleted:
+              (PhoneAuthCredential phoneAuthCredential) async {
+            await _firebaseAuth.signInWithCredential(phoneAuthCredential);
+          },
+          verificationFailed: (error) {
+            throw Exception(error.message);
+          },
+          codeSent: codeSent,
+          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message.toString());
+    }
   }
 }
