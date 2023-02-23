07c09f4623cf97d11f869148e540f7375f264e7c
Vedant Suryawanshi <68188208+VedantSuryawanshi@users.noreply.github.com>
2023-02-23 07:34:17 +0530

    commit

---
+++ lib/utils/pdf_test.dart
@@ -0,0 +1,12 @@
+// import 'package:pdf/pdf.dart';
+// import 'package:pdf/widgets.dart' as pw;
+
+// final pdf = pw.Document();
+
+// pdf.addPage(pw.Page(
+//       pageFormat: PdfPageFormat.a4,
+//       build: (pw.Context context) {
+//         return pw.Center(
+//           child: pw.Text("Hello World"),
+//         ); // Center
+//       })); // Page
