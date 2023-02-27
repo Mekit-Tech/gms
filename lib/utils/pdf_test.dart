37bcae1bf5b1674c7c92587938f12adb79367452
Vedant Suryawanshi <68188208+VedantSuryawanshi@users.noreply.github.com>
2023-02-27 14:09:31 +0530

    commit

---
+++ lib/utils/pdf_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+
+class PdfGeneration extends StatelessWidget {
+  const PdfGeneration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+    ));
+  }
+}
