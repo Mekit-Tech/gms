3662ca5f8c1e96b6083bd35daaf74c4fec706d97
vedantsuryawanshi <hellovedantsuryawanshi@gmail.com>
2024-05-30 08:42:38 +0530

    commin

--- lib/UI/screens/tabs/contacts.dart
+++ lib/UI/screens/tabs/contacts.dart
@@ -1,63 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:mekit_gms/UI/widgets/demo_contact.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class CustomerContacts extends StatelessWidget {
+class CustomerContacts extends StatefulWidget {
   const CustomerContacts({Key? key}) : super(key: key);
 
   @override
+  _CustomerContactsState createState() => _CustomerContactsState();
+}
+
+class _CustomerContactsState extends State<CustomerContacts> {
+  @override
   Widget build(BuildContext context) {
-    return Column(mainAxisSize: MainAxisSize.max, children: [
-      Container(
-        margin: const EdgeInsets.only(left: 30, right: 30, top: 25, bottom: 40),
-        width: double.infinity,
-        height: 50,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                  blurRadius: 10,
-                  offset: const Offset(0, 15),
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: -9),
-              BoxShadow(
-                blurRadius: 7,
-                offset: const Offset(0, 1),
-                color: Colors.grey.withOpacity(0.6),
-              )
-            ]),
-        child: const TextField(
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.all(20),
-            prefixIcon: Padding(
-              padding: EdgeInsets.only(left: 20, right: 10),
-              child: Icon(
-                Icons.search,
-                color: Colors.grey,
-              ),
-            ),
-            hintText: 'Search by name or number',
-            hintStyle: TextStyle(
-              fontSize: 14,
-              color: Colors.grey,
-            ),
-          ),
-        ),
-      ),
-      const Text(
-        "A - Z",
-        style: TextStyle(
-          fontFamily: 'DMSans - Regular',
-          color: Colors.black,
-          fontSize: 15,
-        ),
-      ),
-      const ContactsWidget(),
-      const ContactsWidget(),
-      const ContactsWidget(),
-      const ContactsWidget(),
-      const ContactsWidget(),
-    ]);
+    return FutureBuilder<User?>(
+      future: _getCurrentUser(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        User? user = snapshot.data;
+        String garageId = user!.uid;
+
+        return ContactsWidget(garageId: garageId);
+      },
+    );
+  }
+
+  Future<User?> _getCurrentUser() async {
+    return FirebaseAuth.instance.currentUser;
   }
 }
--- lib/UI/widgets/demo_contact.dart
+++ lib/UI/widgets/demo_contact.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ContactsWidget extends StatelessWidget {
-  const ContactsWidget({Key? key}) : super(key: key);
+  final String garageId;
+
+  const ContactsWidget({Key? key, required this.garageId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,47 +14,57 @@ class ContactsWidget extends StatelessWidget {
       decoration: const BoxDecoration(
         border: Border(top: BorderSide(color: Colors.grey, width: 0.7)),
       ),
-      child: const Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                "Adar Daruwala",
-                style: TextStyle(
-                  fontFamily: 'DMSans - Regular',
-                  color: Colors.black,
-                  fontSize: 18,
-                ),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 5),
-                child: Text(
-                  "Honda City",
-                  style: TextStyle(
-                    fontFamily: 'DMSans - Regular',
-                    color: Colors.black54,
-                    fontSize: 12,
+      child: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('garages')
+            .doc(garageId)
+            .collection('customers')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          var customers = snapshot.data!.docs;
+
+          return Column(
+            children: customers.map((customer) {
+              var customerData = customer.data() as Map<String, dynamic>;
+              return Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        customerData['customer_name'] ?? '',
+                        style: const TextStyle(
+                          fontFamily: 'DMSans - Regular',
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 5),
-                child: Text(
-                  ", Kia Seltos",
-                  style: TextStyle(
-                    fontFamily: 'DMSans - Regular',
-                    color: Colors.black54,
-                    fontSize: 12,
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Text(
+                          customerData['car_model'] ?? '',
+                          style: const TextStyle(
+                            fontFamily: 'DMSans - Regular',
+                            color: Colors.black54,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              )
-            ],
-          )
-        ],
+                  const Divider(color: Colors.grey, height: 20),
+                ],
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
