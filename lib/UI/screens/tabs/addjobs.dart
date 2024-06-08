0b0800ce21643ec43bb5d2109e29449cf3d9a060
vedantsuryawanshi <hellovedantsuryawanshi@gmail.com>
2024-06-08 18:03:15 +0530

    commit

--- lib/UI/screens/tabs/addjobs.dart
+++ lib/UI/screens/tabs/addjobs.dart
@@ -53,153 +53,160 @@ class _AddJobsState extends State<AddJobs> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: StreamBuilder<QuerySnapshot>(
-        stream: _uid.isNotEmpty
-            ? FirebaseFirestore.instance
-                .collection('garages')
-                .doc(_uid)
-                .collection('customers')
-                .snapshots()
-            : null,
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: ListView(
-                children: snapshot.data!.docs.map((customer) {
-                  var customerData = customer.data() as Map<String, dynamic>?;
-
-                  if (customerData == null) {
-                    return const SizedBox.shrink();
-                  }
-
-                  return StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance
-                        .collection('garages')
-                        .doc(_uid)
-                        .collection('customers')
-                        .doc(customer.id)
-                        .collection('jobs')
-                        .where('status', isEqualTo: 'active')
-                        .snapshots(),
-                    builder: (context, jobSnapshot) {
-                      if (jobSnapshot.connectionState ==
-                          ConnectionState.waiting) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      if (jobSnapshot.hasData &&
-                          jobSnapshot.data!.docs.isNotEmpty) {
-                        return Column(
-                          children: jobSnapshot.data!.docs.map((job) {
-                            var jobData = job.data() as Map<String, dynamic>;
-
-                            return GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => VehicleProfile(
-                                      customer: customer,
-                                      jobId: job.id,
-                                    ),
-                                  ),
-                                );
-                              },
-                              child: Container(
-                                margin:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                width: double.infinity,
-                                child: Card(
-                                  elevation: 3,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  color:
-                                      const Color.fromARGB(255, 210, 239, 253),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(12),
-                                    child: Row(
-                                      children: [
-                                        Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              customerData['customer_name'] ??
-                                                  'Unknown',
-                                              style: const TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            const SizedBox(height: 8),
-                                            Text(
-                                              jobData['car_number'] ?? 'N/A',
-                                              style: const TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 16,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        const Spacer(),
-                                        if (customerData
-                                            .containsKey('customer_phone')) ...[
-                                          IconButton(
-                                            icon: const Icon(Icons.call),
-                                            color: Colors.green,
-                                            onPressed: () {
-                                              _makePhoneCall(
-                                                  customerData['phone_number']);
-                                            },
-                                          ),
-                                        ],
-                                        if (customerData
-                                            .containsKey('customer_phone')) ...[
-                                          IconButton(
-                                            icon: const Icon(Icons.message),
-                                            color: Colors.green,
-                                            onPressed: () {
-                                              _openWhatsApp(
-                                                  customerData['phone_number']);
-                                            },
-                                          ),
-                                        ],
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                          }).toList(),
-                        );
-                      } else {
-                        return const Center(
-                          child: Text(""),
-                        );
-                      }
-                    },
+      body: _uid.isNotEmpty
+          ? StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('garages')
+                  .doc(_uid)
+                  .collection('customers')
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
-                }).toList(),
-              ),
-            );
-          } else {
-            return const Center(
-              child: Text(
-                "You have no customers",
-              ),
-            );
-          }
-        },
-      ),
+                }
+                if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: ListView(
+                      children: snapshot.data!.docs.map((customer) {
+                        var customerData =
+                            customer.data() as Map<String, dynamic>?;
+
+                        if (customerData == null) {
+                          return const SizedBox.shrink();
+                        }
+
+                        return StreamBuilder<QuerySnapshot>(
+                          stream: FirebaseFirestore.instance
+                              .collection('garages')
+                              .doc(_uid)
+                              .collection('customers')
+                              .doc(customer.id)
+                              .collection('jobs')
+                              .where('status', isEqualTo: 'active')
+                              .snapshots(),
+                          builder: (context, jobSnapshot) {
+                            if (jobSnapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                            if (jobSnapshot.hasData &&
+                                jobSnapshot.data!.docs.isNotEmpty) {
+                              return Column(
+                                children: jobSnapshot.data!.docs.map((job) {
+                                  var jobData =
+                                      job.data() as Map<String, dynamic>;
+
+                                  return GestureDetector(
+                                    key: ValueKey(job.id),
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => VehicleProfile(
+                                            customer: customer,
+                                            jobId: job.id,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    child: Container(
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 10),
+                                      width: double.infinity,
+                                      child: Card(
+                                        elevation: 3,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        color: const Color.fromARGB(
+                                            255, 210, 239, 253),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(12),
+                                          child: Row(
+                                            children: [
+                                              Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    customerData[
+                                                            'customer_name'] ??
+                                                        'Unknown',
+                                                    style: const TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 18,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(height: 8),
+                                                  Text(
+                                                    jobData['car_number'] ??
+                                                        'Unknown Car Number',
+                                                    style: const TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 16,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              const Spacer(),
+                                              if (customerData.containsKey(
+                                                  'customer_phone')) ...[
+                                                IconButton(
+                                                  icon: const Icon(Icons.call),
+                                                  color: Colors.green,
+                                                  onPressed: () {
+                                                    _makePhoneCall(customerData[
+                                                        'customer_phone']);
+                                                  },
+                                                ),
+                                                IconButton(
+                                                  icon:
+                                                      const Icon(Icons.message),
+                                                  color: Colors.green,
+                                                  onPressed: () {
+                                                    _openWhatsApp(customerData[
+                                                        'customer_phone']);
+                                                  },
+                                                ),
+                                              ],
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }).toList(),
+                              );
+                            } else {
+                              return const Center(
+                                child: Text(""),
+                              );
+                            }
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: Text(
+                      "You have no customers",
+                    ),
+                  );
+                }
+              },
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
--- lib/UI/screens/tabs/contacts.dart
+++ lib/UI/screens/tabs/contacts.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mekit_gms/UI/screens/customerdetails.dart'; // Import EditCustomerDetails screen
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomerContacts extends StatefulWidget {
   const CustomerContacts({Key? key}) : super(key: key);
@@ -11,8 +12,19 @@ class CustomerContacts extends StatefulWidget {
 }
 
 class _CustomerContactsState extends State<CustomerContacts> {
+  String _searchQuery = '';
   Future<User?> _getCurrentUser() async {
     return FirebaseAuth.instance.currentUser;
+  }
+
+  void _makePhoneCall(String phoneNumber) async {
+    final sanitizedPhoneNumber = phoneNumber.replaceAll(RegExp(r'\s+'), '');
+    final Uri url = Uri(scheme: 'tel', path: sanitizedPhoneNumber);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      print('Could not launch $url');
+    }
   }
 
   @override
@@ -27,67 +39,90 @@ class _CustomerContactsState extends State<CustomerContacts> {
         User? user = snapshot.data;
         String garageId = user!.uid;
 
-        return StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('garages')
-              .doc(garageId)
-              .collection('customers')
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            var customers = snapshot.data!.docs;
-
-            return ListView.builder(
-              itemCount: customers.length,
-              itemBuilder: (context, index) {
-                var customer = customers[index];
-                var customerData = customer.data() as Map<String, dynamic>;
-
-                return Card(
-                  elevation: 4.0,
-                  margin: const EdgeInsets.all(10.0),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CustomerDetails(
-                            garageId: garageId,
-                            customerId: customer.id,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            customerData['customer_name'] ?? 'N/A',
-                            style: const TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            customerData['car_number'] ?? 'N/A',
-                            style: const TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
+        return Scaffold(
+          appBar: AppBar(
+            title: TextField(
+              decoration: const InputDecoration(
+                hintText: 'Search customers...',
+                border: InputBorder.none,
+                hintStyle: TextStyle(color: Colors.white70),
+              ),
+              style: const TextStyle(color: Colors.white),
+              onChanged: (query) {
+                setState(() {
+                  _searchQuery = query.toLowerCase();
+                });
               },
-            );
-          },
+            ),
+            backgroundColor: Colors.blue,
+          ),
+          body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('garages')
+                .doc(garageId)
+                .collection('customers')
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              var customers = snapshot.data!.docs.where((doc) {
+                var customerData = doc.data() as Map<String, dynamic>;
+                var customerName = customerData['customer_name'] ?? '';
+                return customerName
+                    .toString()
+                    .toLowerCase()
+                    .contains(_searchQuery);
+              }).toList();
+
+              return ListView.builder(
+                itemCount: customers.length,
+                itemBuilder: (context, index) {
+                  var customer = customers[index];
+                  var customerData = customer.data() as Map<String, dynamic>;
+
+                  return Card(
+                    elevation: 2.0,
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 5.0),
+                    child: ListTile(
+                      title: Text(
+                        customerData['customer_name'] ?? 'N/A',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        customerData['car_number'] ?? 'Unknown Car Number',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.call, color: Colors.green),
+                        onPressed: () {
+                          _makePhoneCall(customerData['phone_number']);
+                        },
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CustomerDetails(
+                              garageId: garageId,
+                              customerId: customer.id,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            },
+          ),
         );
       },
     );
